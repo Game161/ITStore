8f2124ba3c899d63c882cbe59a1da8fc6936dade
BloodyRegina <thanwapermpool@gmail.com>
2024-09-08 13:27:36 +0700

    congrat

--- src/elements/Slideshow.jsx
+++ src/elements/Slideshow.jsx
@@ -22,7 +22,10 @@ const Slideshow = () => {
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [progress, setProgress] = useState(Array(slides.length).fill(0)); // Progress array for each slide
 
+  const slideDuration = 3000; // Duration each slide is shown in ms
+  const progressInterval = 100; // Update interval for progress bar in ms
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
@@ -39,9 +42,28 @@ const Slideshow = () => {
     setCurrentIndex(slideIndex);
   };
   useEffect(() => {
-    const interval = setInterval(nextSlide, 3000);
-    return () => clearInterval(interval); // ล้าง interval เมื่อ component ถูก unmount หรือ currentIndex เปลี่ยน
-  }, [currentIndex]); // เพิ่ม currentIndex เป็น dependency
+    // Set progress to 0 for all slides
+    setProgress(Array(slides.length).fill(0));
+
+    const progressUpdateInterval = setInterval(() => {
+      setProgress((prevProgress) => {
+        const newProgress = [...prevProgress];
+        newProgress[currentIndex] = (newProgress[currentIndex] + (100 / (slideDuration / progressInterval))) % 100;
+        return newProgress;
+      });
+    }, progressInterval);
+
+    const slideChangeTimeout = setTimeout(() => {
+      nextSlide();
+    }, slideDuration);
+
+    return () => {
+      clearInterval(progressUpdateInterval);
+      clearTimeout(slideChangeTimeout);
+    };
+  }, [currentIndex]);
+
+
   return (
     <div className="h-screen w-full relative group">
       <div
@@ -63,87 +85,13 @@ const Slideshow = () => {
             onClick={() => goToSlide(slideIndex)}
             className="text-2xl cursor-pointer"
           >
-            <span className="progress"></span>
-            <div className="rhombus1 step4 ml-2 bg-slate-200"></div>
+            <div className="rhombus1 step4 ml-2 bg-slate-200" >
+              <div id="length"></div>
+              <div id="progress_animation" style={{ width: `${progress[slideIndex]}%`, transition: `width ${progressInterval}ms linear` }}></div>
+            </div>
           </div>
         ))}
       </div>
-
-      {/* e.prototype.mounted = function(){
-                t = this;
-                window.addEventListener("resize", (function() {
-                    t.checkMobileStatus()
-                }
-                )),
-                this.checkMobileStatus(),
-                setTimeout((function() {
-                    t.nowPointNumber = 0,
-                    t.autoSlideShowHandler(),
-                    t.progressBarHandler()
-                }
-                ), 1500),
-                this.defaultX = window.innerWidth / 2
-              }
-            ,
-            e.prototype.progressBarHandler = function() {
-                t = this;
-                this.progressTimer = 0,
-                this.slideController = !0,
-                this.progressTime = this.randomTime / 100,
-                this.progressPercent = 0,
-                this.progressTimer = setInterval((function() {
-                    !0 === t.slideController && t.progressPercent < 100 && t.progressPercent++,
-                    t.progressPercent >= 100 ? clearInterval(t.progressTimer) : t.progressTimer = null
-                }
-                ), this.progressTime)
-            }
-            ,
-            e.prototype.reAutoHandler = function() {
-                t = this;
-                this.reAutoInterval = setInterval((function() {
-                    t.reAutoStatus && (t.autoSlideShowHandler(),
-                    t.reAutoStatus = !1)
-                }
-                ), 0)
-            }
-            ,
-            e.prototype.autoSlideShowHandler = function() {
-                t = this
-                  , e = Object.keys(this.banner).length;
-                this.autoStatus = setInterval((function() {
-                    1 === t.bannerLength && (clearInterval(t.autoStatus),
-                    clearInterval(t.progressTimer),
-                    t.reAutoStatus = !1,
-                    t.slideController = !1),
-                    t.nowPointNumber === e - 1 ? (t.moveDelta = 0,
-                    t.nowNumber = 0,
-                    t.nowPointNumber = t.nowNumber,
-                    t.progressBarHandler(),
-                    clearInterval(t.progressTimer)) : (t.moveDelta = t.moveDelta - 100,
-                    t.nowNumber++,
-                    t.nowPointNumber = t.nowNumber,
-                    t.progressBarHandler(),
-                    clearInterval(t.progressTimer))
-                }
-                ), this.randomTime)
-            }
-            e.prototype.switchSlide = function() {
-                this.isShowPlayButton ? this.playSlide() : this.pauseSlide()
-            }
-            ,
-            e.prototype.playSlide = function() {
-                this.reAutoHandler(),
-                this.reAutoStatus = !0,
-                this.slideController = !0,
-                this.isShowPlayButton = !1
-            }
-            ,
-            e.prototype.pauseSlide = function() {
-                clearInterval(this.autoStatus),
-                this.reAutoStatus = !1,
-                this.slideController = !1,
-                this.isShowPlayButton = !0
-            } */}
     </div>
   );
 };
